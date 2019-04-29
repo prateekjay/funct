45bc862f45e33832e5755df56ea5c9993b1bc051
prateekj <prateekj@subhikksha.com>
2019-04-29 16:05:49 +0530

    app related

---
+++ index.js
@@ -0,0 +1,19 @@
+
+const Task = require('data.task')
+const Spotify = require('./spotify')
+const argv = new Task((rej,res) => res(process.argv))
+const names = argv.map(args => args.slice(2))
+
+
+const related = name =>  
+						Spotify.findArtist(name)
+						.map(artist => artist.id)
+						.chain(Spotify.relatedArtists)
+						.map(artists => artists.map(artist => artist.name))
+
+
+const main = ([name1,name2]) =>
+			Task.of(rels1 => rels2 => [rels1,rels2])
+			.ap(related(name1)).ap(related(name2))
+
+names.chain(main).fork(console.err,console.log)
